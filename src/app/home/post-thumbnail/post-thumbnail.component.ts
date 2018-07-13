af899b043ce3da72aaa71a9724358db45398d365
Ziad Alzarka <zizohotot@gmail.com>
2018-07-13 20:14:31 +0200

    home

--- src/app/home/post-thumbnail/post-thumbnail.component.ts
+++ src/app/home/post-thumbnail/post-thumbnail.component.ts
@@ -1,15 +1,15 @@
 import { Component, OnInit } from '@angular/core';
 
 @Component({
-  selector: 'app-post-thumbnail',
-  templateUrl: './post-thumbnail.component.pug',
-  styleUrls: ['./post-thumbnail.component.css']
+	selector: 'app-post-thumbnail',
+	templateUrl: './post-thumbnail.component.pug',
+	styleUrls: ['./post-thumbnail.component.css']
 })
 export class PostThumbnailComponent implements OnInit {
 
-  constructor() { }
+	constructor() { }
 
-  ngOnInit() {
-  }
+	ngOnInit() {
+	}
 
 }
