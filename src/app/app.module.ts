714e159019bd1b468f6dd74da7a18fd294bb0c1f
Ziad Alzarka <zizohotot@gmail.com>
2018-07-14 20:16:06 +0200

    extracted materialize scripts

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,7 +1,6 @@
 import { GlobalModule } from './global/global.module';
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
