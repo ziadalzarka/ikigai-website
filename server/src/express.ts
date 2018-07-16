6fcc958ad23a0401a302fa1da8702fe36b9ad99e
Ziad Alzarka <zizohotot@gmail.com>
2018-07-16 19:41:09 +0200

    enabled express compression

--- server/src/express.ts
+++ server/src/express.ts
@@ -2,6 +2,7 @@ import * as express from 'express';
 import * as cookieParser from 'cookie-parser';
 import * as bodyParser from 'body-parser';
 import * as cors from 'cors';
+import * as compression from 'compression';
 
 import webRoutes from './routes/website';
 
@@ -12,6 +13,7 @@ const app = express();
 app.use(cors());
 app.use(cookieParser());
 app.use(bodyParser.json());
+app.use(compression());
 app.use(webRoutes);
 
 app.listen(PORT, () => {
