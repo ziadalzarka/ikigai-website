4a324213696c27a95d5f22a8db0db9631aa8c4fe
Ziad Alzarka <zizohotot@gmail.com>
2018-07-18 01:50:51 +0200

    environment

--- server/src/resolvers/Query.ts
+++ server/src/resolvers/Query.ts
@@ -1,28 +1,28 @@
-import { getUserId, Context } from '../utils'
+import { getUserId, Context } from '../utils';
 
 export const Query = {
 	feed(parent, args, ctx: Context, info) {
-		return ctx.db.query.posts({}, info)
+		return ctx.db.query.posts({}, info);
 	},
 
 	drafts(parent, args, ctx: Context, info) {
-		const id = getUserId(ctx)
+		const id = getUserId(ctx);
 
 		const where = {
 			author: {
 				id
 			}
-		}
+		};
 
-		return ctx.db.query.posts({ where }, info)
+		return ctx.db.query.posts({ where }, info);
 	},
 
 	post(parent, { id }, ctx: Context, info) {
-		return ctx.db.query.post({ where: { id } }, info)
+		return ctx.db.query.post({ where: { id } }, info);
 	},
 
 	me(parent, args, ctx: Context, info) {
 		const id = getUserId(ctx)
-		return ctx.db.query.user({ where: { id } }, info)
+		return ctx.db.query.user({ where: { id } }, info);
 	},
-}
+};
--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,6 @@
 export const environment = {
-  production: true
+	production: true,
+	graphql: {
+		url: 'http://185.33.146.23:4090'
+	}
 };
