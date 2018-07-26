386fb96ef95552ca28d66892bccd4cbdc520112d
Ziad Alzarka <zizohotot@gmail.com>
2018-07-26 23:29:52 +0200

    seo tags

--- src/app/seo.service.ts
+++ src/app/seo.service.ts
@@ -32,13 +32,13 @@ export class SeoService {
 		this.meta.updateTag({ name: 'description', content: config.description });
 		this.meta.updateTag({ name: 'image', content: config.image });
 
-		this.meta.updateTag({ name: 'twitter:card', content: 'summary' });
+		this.meta.updateTag({ name: 'twitter:card', content: 'summary_large_image' });
 		this.meta.updateTag({ name: 'twitter:site', content: '@ikigai' });
 		this.meta.updateTag({ name: 'twitter:title', content: config.title });
 		this.meta.updateTag({ name: 'twitter:description', content: config.description });
 		this.meta.updateTag({ name: 'twitter:image', content: config.image });
 
-		this.meta.updateTag({ property: 'og:type', content: 'article' });
+		this.meta.updateTag({ property: 'og:type', content: 'website' });
 		this.meta.updateTag({ property: 'og:site_name', content: 'Ikigai' });
 		this.meta.updateTag({ property: 'og:title', content: config.title });
 		this.meta.updateTag({ property: 'og:description', content: config.description });
